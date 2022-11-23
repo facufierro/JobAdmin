6ed278603a0be1209230c38ec3ce9a25d60fc43f
Facundo Fierro <fierrof_47@hotmail.com>
2022-11-23 11:40:08 +0100

    rearrenged database

--- src/jobmanager/models.py
+++ src/jobmanager/models.py
@@ -34,12 +34,12 @@ class Employee(models.Model):
     def __str__(self):
         return self.name
 
-
-class JobEmployee(models.Model):
+class WorkedHours(models.Model):
     id = models.AutoField(primary_key=True)
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+
     def __str__(self):
-        return self.job.title + ' - ' + self.employee.name
+        return self.employee.name + ' - ' + self.job.title + ' - ' + str(self.hours)
